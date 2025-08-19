600b37d43a3d4b15934a41560f163863ea2e79c6
Raphael Mitas <raphael.mitas@web.de>
2025-08-19 22:43:16 +0200

    www

--- apps/expo/src/utils/base-url.ts
+++ apps/expo/src/utils/base-url.ts
@@ -17,7 +17,7 @@ export const getBaseUrl = () => {
   const localhost = debuggerHost?.split(":")[0];
 
   if (!localhost) {
-    return "https://flatsby.com";
+    return "https://www.flatsby.com";
   }
   return `http://${localhost}:3000`;
 };
