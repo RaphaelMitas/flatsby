b0b4fee04e630e8a628d229371857c2dea9b5dc8
Raphael Mitas <raphael.mitas@web.de>
2025-08-19 22:48:53 +0200

    flatcove for build

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -3,8 +3,8 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Flatsby",
-  slug: "flatsby",
-  scheme: "flatsby",
+  slug: "flatcove",
+  scheme: "flatcove",
   owner: "frenchrapho",
   version: "0.4.0",
   orientation: "portrait",
@@ -23,7 +23,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     infoPlist: { ITSAppUsesNonExemptEncryption: false },
   },
   android: {
-    package: "com.flatsby.app",
+    package: "com.flatcove.app",
     blockedPermissions: ["android.permission.READ_PHONE_STATE"],
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
