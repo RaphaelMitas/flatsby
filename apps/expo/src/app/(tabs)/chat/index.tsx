73f950b970159d33412880347ace13424536aac6
Raphael Mitas <raphael.mitas@web.de>
2026-03-29 05:29:21 +0200

    REMOVE UNUSED

--- apps/expo/src/app/(tabs)/chat/index.tsx
+++ apps/expo/src/app/(tabs)/chat/index.tsx
@@ -1,15 +1,11 @@
-import { View } from "react-native";
-
 import { AIConsentGate } from "~/components/chat/AIConsentGate";
 import { ChatSplitView } from "~/components/chat/ChatSplitView";
 
 const ChatIndex = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <AIConsentGate>
-        <ChatSplitView />
-      </AIConsentGate>
-    </View>
+    <AIConsentGate>
+      <ChatSplitView />
+    </AIConsentGate>
   );
 };
 
